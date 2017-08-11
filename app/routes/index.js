6eeb54ec9512b75f7e1b4d0a27f538897a05952b
Steve Zaske <github@zaske.com>
2017-08-11 11:19:56 -0700

    Show list of questions on the home page, with links to the individual question pages

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question').then(function(results) {
+        return results.sortBy('timestamp'); }),
+      answers: this.store.findAll('answer')
+    });
+  }
 });
